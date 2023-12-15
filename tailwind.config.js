0193cee02f9c4c3c10d9184f18b46ee93408b98e
sulosp <59638018+sulosp@users.noreply.github.com>
2023-12-16 00:10:25 +0530

    v1

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            cyan: '#5590F5',
+            blue:'#44BFFF',
+
+          }
+         
+        }
+
+        
+        
+      }
+    }
